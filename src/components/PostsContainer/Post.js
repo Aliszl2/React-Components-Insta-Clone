1b06bea1a4d7e63162d4d40c2d3e0593a851d9ad
Alison Law <alisparklaw@gmail.com>
2020-01-21 14:31:49 +0000

    Likes working with state data from dummy-data

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -9,7 +9,7 @@ import "./Posts.css";
 
 const Post = props => {
   // set up state for the likes
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState(props.post.likes);
   return (
     <div className="post-border">
       <PostHeader
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -7,6 +7,7 @@ import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
 const PostsPage = (props) => {
+  console.log(props);
   console.log(props.data);
   return (
     <div className="posts-container-wrapper">
